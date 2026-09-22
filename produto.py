d88645e053159a7dcd1b8c815fb11cd2249cf28f
Manuella Ribeiro <manuritasribeiro@gmail.com>
2026-09-21 21:57:41 -0300

    ajustes no programa do cadastro de produtos

--- produto.py
+++ produto.py
@@ -3,77 +3,98 @@ import colorama
 from colorama import Fore, Back, Style
 colorama.init(autoreset=True) 
 
+
 def menu():
     print()
     print(Fore.CYAN + Style.BRIGHT + "====== MENU ======")
     print("1- cadastar produtos \n2-listar produtos \n0-Encerrar programa \n")
-while True:
-    try:
-        escolha = int(input("O que você deseja fazer?  "))
-        if escolha in [0, 1, 2]:
-           return escolha 
-    except ValueError:
-        print(Fore.RED + "Digite apenas um número!")
-        
-    print(Fore.YELLOW + "Opção inválida! Digite 0, 1 ou 2.")
-
-def cadastrar(arquivo):
-    print()
-    print("\n ====== CADASTRO DE PRODUTOS ====== \n ")
-
-while True:
-    nome_produto= input("Nome do produto: ")
-    if nome_produto.strip():
-        break
-    print(Fore.RED + "O nome do produto não pode estar vazio!")
-        continue 
 
     while True:
         try:
-            preco = float(input("preço do produto: "))
-            preco = float(preco.replace(",""."))
-            if preco <0:
-                break
-                print(Fore.RED + "O preço deve ser maior que zero (0)")
+            escolha = int(input("O que você deseja fazer? "))
+            if escolha in [0, 1, 2]:
+                return escolha 
+
+            if escolha < 0 or escolha>3:
+                print(Fore.RED + "Opção inválida! Digite 0, 1 ou 2. \n")
+
         except ValueError:
-            print(Fore.RED + "Digite um preço válido!")
-                continue
-                
+            #print(Fore.RED + "\nDigite apenas um número!")
+            print(Fore.RED + "Opção inválida! Digite 0, 1 ou 2. \n")
+            
+    
+def cadastrar(arquivo):
+    
+    print(Fore.GREEN + "\n====== CADASTRO DE PRODUTOS ====== \n ")
+
+    while True:
+        nome_produto= input("Nome do produto: ")
+        if nome_produto.strip():
             break
-        except ValueError: 
-        print(Fore.RED + "Digite um preço válido!")
+        print(Fore.RED + "O nome do produto não pode estar vazio! \n")
+        continue 
+                         
+    while True:
+        try:
+            preco = float(input("preço do produto: ").replace("," , "."))
+            
+            if preco <0:
+                print(Fore.RED + "O preço deve ser maior que zero (0) \n")
+                continue
+
+            else:
+                break
+
+        except ValueError:
+            print(Fore.RED + "Digite um preço válido! \n")
+            continue
+                
+            #break
+        #except ValueError: 
+            #print(Fore.RED + "Digite um preço válido!")
         
     while True: 
         try: 
             quantidade= int(input("quantidade de produtos:"))
-            if quantidade >= 0:
+
+            if quantidade <= 0:
+                #break
+                print(Fore.RED + "A quantidade deve ser maior que zero (0)")
+                continue
+
+            else:
                 break
-                print:(Fore.RED + "A quantidade deve ser maior que zero (0)")
+                
         except ValueError:
-            print(Fore.RED + "Digite uma quantidade válida!")
+            print(Fore.RED + "Digite uma quantidade válida! \n")
+            continue
 
     with open (arquivo, "a", encoding="utf-8") as dado:
         dado.write(
             f"{nome_produto}, {preco}, {quantidade}\n"
         )
-            print(Fore.GREEN + "Produto cadastrado com sucesso!")
+        print(Fore.GREEN + "Produto cadastrado com sucesso!")
+
+        
 def listar_produtos(arquivo):
     print()
     print(Fore.GREEN + "====== PRODUTOS CADASTRADOS ====== \n")
 
-encontrou = False
-try:
-    with open(arquivo, "r", encoding="utf-8") as dados:
-        for linha in dados:
-            lista=linha.split(", ")
-            encontrou = True
-            print(f"{Fore.YELLOW}Produto: {lista[0]} ")
-            print(f"{Fore.YELLOW}Preço: {lista[1]}")
-            print(f"{Fore.YELLOW}Quantidade: {lista[2]} ")
-        if not encontrou:
-            print(Fore.YELLOW + "Nenhum produto cadastrado ainda!")
-except fileNotFoundError:
-    print(Fore.RED + "Nenhum produto cadartrado ainda!")
+    encontrou = False
+    try:
+        with open(arquivo, "r", encoding="utf-8") as dados:
+            for linha in dados:
+                lista=linha.split(", ")
+                encontrou = True
+                print(f"{Fore.YELLOW}Produto: {lista[0]} ")
+                print(f"{Fore.YELLOW}Preço: {lista[1]}")
+                print(f"{Fore.YELLOW}Quantidade: {lista[2]} ")
+                
+            if not encontrou:
+                print(Fore.YELLOW + "Nenhum produto cadastrado ainda!")
+    except FileNotFoundError:
+        print(Fore.RED + "Nenhum produto cadartrado ainda!")
+
     
 arquivo= r"C:\Users\manur\OneDrive\Documentos\EM 1DS\PA\trabalho em grupo\Desenvolvimento-colaborativo-com-git-e-github\produtos.txt"
 
@@ -89,9 +110,9 @@ while True:
     if escolha == 0:
         break
 
-while True: 
-    try:
-    escolha = int(input("O que você deseja fazer? "))
-    return escolha
-except valueError:
-    print(Fore.RED + "Digite apenas um número!")
+#while True: 
+    #try:
+        #escolha = int(input("O que você deseja fazer? "))
+    #return escolha
+    #except valueError:
+    #print(Fore.RED + "Digite apenas um número!")
